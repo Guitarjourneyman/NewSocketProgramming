359dc926baaef2d0b5a8fc3f0560bcdb8dc4fe6e
Guitarjourneyman <149582970+Guitarjourneyman@users.noreply.github.com>
2024-11-08 09:56:21 +0900

    Add files via upload

---
+++ src/src/Server_Tcp.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.Socket;
+import javax.swing.JTextArea;
+
+public class Server_Tcp {
+    /* 메시지를 받기만 하는 기능 구현 */
+    private Socket socket;
+    private static final int PORT = 8189;
+    private JTextArea receivedMessagesArea;  // GUI의 receive message 창
+    private volatile boolean newAckReceived_tcp = false; // 에코 메시지 수신 여부
+    private int receive_message_num = 0;
+
+    // 생성자에서 JTextArea 전달 받음
+    public Server_Tcp(Socket socket, JTextArea receivedMessagesArea) {
+        this.socket = socket;
+        this.receivedMessagesArea = receivedMessagesArea;
+    }
+    
+    public void reset_message_num() {
+        receive_message_num = 0;
+    }
+    
+    public boolean hasNewEchoMessage() {
+        return newAckReceived_tcp;
+    }
+
+    public void resetNewEchoMessageFlag() {
+        newAckReceived_tcp = false;
+    }
+
+    public void startReceiving() throws IOException { //예외 throw하여 ClientHandler에서 처리하도록함
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try {
+            // BufferedReader를 사용하여 데이터를 송수신
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String clientIP = socket.getInetAddress().getHostAddress();
+            System.out.println("Server_TCP is open");
+
+            // 클라이언트와의 연결을 유지하면서 메시지를 지속적으로 수신
+            String receivedMessage;
+            while (!socket.isClosed() && (receivedMessage = in.readLine()) != null) {
+                // 수신된 메시지 처리
+                receive_message_num++;
+                receivedMessagesArea.append("[" + receive_message_num + "] 수신된 메시지 from " + clientIP + ": " + receivedMessage + "\n");
+                System.out.println("수신된 메시지 from " + clientIP + ": " + receivedMessage);
+                
+                synchronized (this) {
+                    newAckReceived_tcp = true; // 에코 메시지를 받았을 경우
+                    System.out.println("newAckMessage was coming");
+                    notifyAll();
+                }
+            }
+        } finally {
+            // Exception을 throw하여 외부에서 처리하도록 함.
+            if (in != null) in.close();
+            if (out != null) out.close();
+            if (socket != null && !socket.isClosed()) socket.close();
+            System.out.println("TCP 소켓이 닫혔습니다.");
+        }
+    }
+}
---
+++ src/src/TcpConnectionAccepter.java
@@ -0,0 +1,188 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.ClosedByInterruptException;
+import java.util.ArrayList;
+import javax.swing.JTextArea;
+
+public class TcpConnectionAccepter implements Runnable {
+    private static final int PORT = 8189; // 수신할 포트
+    private ServerSocket serverSocket;
+    
+    private JTextArea receivedMessagesArea;
+    private JTextArea consoleArea;
+    private Thread clientHandlerThread;
+    
+    // 모든 ClientHandler 인스턴스를 저장할 리스트 -> ClientInfo에 clientHandler를 넣는게 나을 듯
+    private final ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    
+    public TcpConnectionAccepter(JTextArea receivedMessagesArea, JTextArea consoleArea) {
+        this.receivedMessagesArea = receivedMessagesArea;
+        this.consoleArea = consoleArea;
+    }
+    
+
+    public void run() {
+        
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Waiting for connection...");
+
+            // 클라이언트 연결을 대기하면서, 각 연결에 대해 새로운 스레드를 생성
+            
+            
+            while (!Thread.currentThread().isInterrupted()) {
+                Socket clientSocket = serverSocket.accept(); // 클라이언트 연결 수락
+                
+                /*UDP broad로 Server쪽 IP 전송하는 매커니즘 추가*/
+                String clientIP = clientSocket.getInetAddress().getHostAddress();
+                System.out.println("Client connected: " + clientIP);
+                consoleArea.append("Client connected: " + clientIP+"\n");
+              
+                // 각 클라이언트에 대해 새로운 핸들러 스레드를 생성
+                TcpConnectionManager.addClient(clientSocket.getInetAddress().getHostAddress(),clientSocket,true,false);                
+                ClientHandler clientHandler = new ClientHandler(clientSocket, this, receivedMessagesArea);
+                //clientHandler객체를 담아두는 리스트 생성 -> 모든 객체를 반납하기위해서 만듦
+                clientHandlers.add(clientHandler);
+                clientHandlerThread = new Thread(clientHandler);
+                clientHandlerThread.start();
+                
+                
+                	
+                
+            }
+            //consoleArea.append("TCP 소켓 연결완료 \n");
+            //System.out.println("All TCP Sockets are connnected");
+            
+            
+        
+        } catch (IOException e) {
+        	if(Thread.currentThread().isInterrupted()) {
+            	//Interrupt가 발생하면 while문 종료
+            	//스레드가 죽기전 모든 clientHandler를 종료시킨다.
+            	System.out.println("Program is clear by Thread Interruption");
+            	killAllClientHandlers();  
+            	
+            	
+            }
+            e.printStackTrace();            
+        }
+        
+    }
+ // 모든 ClientHandler의 stopHandler를 호출하여 종료
+    private void killAllClientHandlers() {
+        for (ClientHandler handler : clientHandlers) {
+        	System.out.println("Handler of index "+ handler.permanent_id + " is killed");
+            handler.stopHandler();
+        }
+        clientHandlers.clear(); // 리스트를 비워줌
+    }
+    
+    
+    public void closeTcpSocket() {
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+                System.out.println("TCP socket closed successfully.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error closing TCP socket: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    
+    
+    
+
+    public class ClientHandler implements Runnable  {
+        private final Server_Tcp receiverTcp;
+        public int permanent_id;
+        private Socket clientSocket;
+        private boolean running = true; // Socket이 열려있는지 나타내는 변수
+        
+        private StartTCPCheckThread tcpCheckThread;
+        private Thread checkThread;
+        
+        public ClientHandler (Socket clientSocket, TcpConnectionAccepter tcpAccepter, JTextArea receivedMessagesArea) {
+            this.clientSocket = clientSocket;
+
+            // 인덱스를 설정하고, 연결된 클라이언트를 처리할 수 있도록 설정
+            synchronized (NewSocket.tcpconnectionmanager) {
+                if (NewSocket.clients_tcp_index == 0) {
+                    permanent_id = NewSocket.clients_tcp_index;
+                    System.out.println("Client index 0 is added");
+                    
+                    NewSocket.clients_tcp_index++;
+                } else {
+                    permanent_id = NewSocket.clients_tcp_index;
+                    System.out.println("Client index: "+permanent_id+ " is added");
+                    
+                    NewSocket.clients_tcp_index++;
+                }
+                System.out.println("Client: " + clientSocket.getInetAddress() + " is connected by TCP"
+                        + " & index: " + (NewSocket.clients_tcp_index - 1));
+            }
+
+            this.receiverTcp = new Server_Tcp(clientSocket, receivedMessagesArea);
+        }
+
+        @Override
+        public void run() {
+            try {
+                // TCP Ack 메시지를 수신하였는지 체크하는 스레드 생성
+            	tcpCheckThread = new StartTCPCheckThread(receiverTcp, this);
+                checkThread = new Thread(tcpCheckThread);
+                checkThread.start();
+                
+                // TCP Ack 수신  시작
+                receiverTcp.startReceiving();
+
+                // Check for socket status in a loop
+                while (running) {
+                    if (clientSocket.isClosed() || !clientSocket.isConnected()) {
+                        System.out.println("Client socket is closed or disconnected. Stopping handler for client: " + clientSocket.getInetAddress());
+                        break; // Exit the loop if the socket is closed or disconnected
+                    }
+
+                    // 스레드 복잡도를 낮추기위해 1초마다 검사
+                    Thread.sleep(1000);
+                }
+                
+            }catch (IOException e) {
+                System.out.println("IOException in ClientHandler: " );
+                //e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Exception in ClientHandler: " + e.getMessage());
+                e.printStackTrace();
+                
+            } finally {
+                // while문을 빠져나오면 Handler를 종료함
+                
+                stopTCPCheckThread();
+                stopHandler();
+            }
+        }
+
+        // ClientHandler 스레드를 종료하는 메소드 
+        public void stopHandler() {
+            running = false;
+            try {
+                
+                    clientSocket.close();
+                    System.out.println("Client socket closed for client: " + clientSocket.getInetAddress());
+                
+            }catch (ClosedByInterruptException e) {
+            	System.out.println("Thread ends by Interruption");
+            }catch (IOException e) {
+                System.out.println("Error closing client socket: " + e.getMessage());
+            } 
+        }
+        public void stopTCPCheckThread() {
+        	//checkThread.interrupt();
+        	//TCPCheckThread를 중단시킴
+        	tcpCheckThread.killThread();
+        }
+    }
+
+}
